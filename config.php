781e4f08722e129fb14185fe6911f69e1dbab05f
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2023-03-22 09:41:28 -0300

    add environment variables

--- config.php
+++ config.php
@@ -1,11 +1,17 @@
 <?php
   session_start();
 
-  $baseURL = 'http://localhost/devsbook-oo';
-  $db_name = 'devsbook';
-  $db_host = 'localhost';
-  $db_user = 'root';
-  $db_pass = '';
+  $parsed = parse_ini_file('./environment.ini', true);
+  $_ENV['ENVIRONMENT'] = $parsed['ENVIRONMENT'];
+  foreach($parsed[$parsed['ENVIRONMENT']] as $key => $value) {
+    $_ENV[$key] = $value;
+  }
+
+  $baseURL = $_ENV['BASE_URL'];
+  $db_name = $_ENV['DB_NAME'];
+  $db_host = $_ENV['DB_HOST'];
+  $db_user = $_ENV['DB_USER'];
+  $db_pass = $_ENV['DB_PASS'];
 
   $pdo = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_pass);
 ?>
