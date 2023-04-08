a602a8839f99b87ea9f702f8caa3920d72f5cae2
Jean Fernandes de Macedo <jfmacedo91@gmail.com>
2023-04-07 22:52:44 -0300

    settings_action completed

--- settings_action.php
+++ settings_action.php
@@ -58,6 +58,102 @@
       }
     }
 
+    if(isset($_FILES['avatar']) && !empty($_FILES['avatar']['tmp_name'])) {
+      $newAvatar = $_FILES['avatar'];
+      if(in_array($newAvatar['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
+        $avatarWidth = 200;
+        $avatarHeight = 200;
+
+        [$originalWidth, $originalHeight] = getimagesize($newAvatar['tmp_name']);
+        $ratio = $originalWidth / $originalHeight;
+
+        $newWidth = $avatarWidth;
+        $newHeight = $newWidth / $ratio;
+
+        if($newHeight < $avatarHeight) {
+          $newHeight = $avatarHeight;
+          $newWidth = $newHeight * $ratio;
+        }
+
+        $x = $avatarWidth - $newWidth;
+        $y = $avatarHeight - $newHeight;
+
+        $x = $x < 0 ? $x / 2 : $x;
+        $y = $y < 0 ? $y / 2 : $y;
+
+        $finalImage = imagecreatetruecolor($avatarWidth, $avatarHeight);
+        switch($newAvatar['type']) {
+          case 'image/jpeg':
+          case 'image/jpg':
+            $image = imagecreatefromjpeg($newAvatar['tmp_name']);
+          break;
+          case 'image/png':
+            $image = imagecreatefrompng($newAvatar['tmp_name']);
+          break;
+        }
+
+        imagecopyresampled(
+          $finalImage, $image,
+          $x, $y, 0, 0,
+          $newWidth, $newHeight, $originalWidth, $originalHeight
+        );
+
+        $avatarName = md5(time().mt_rand(0, 9999)).'.jpg';
+
+        imagejpeg($finalImage, './media/avatars/'.$avatarName, 100);
+
+        $user->avatar = $avatarName;
+      }
+    }
+
+    if(isset($_FILES['cover']) && !empty($_FILES['cover']['tmp_name'])) {
+      $newCover = $_FILES['cover'];
+      if(in_array($newCover['type'], ['image/jpeg', 'image/jpg', 'image/png'])) {
+        $coverWidth = 850;
+        $coverHeight = 310;
+
+        [$originalWidth, $originalHeight] = getimagesize($newCover['tmp_name']);
+        $ratio = $originalWidth / $originalHeight;
+
+        $newWidth = $coverWidth;
+        $newHeight = $newWidth / $ratio;
+
+        if($newHeight < $coverHeight) {
+          $newHeight = $coverHeight;
+          $newWidth = $newHeight * $ratio;
+        }
+
+        $x = $coverWidth - $newWidth;
+        $y = $coverHeight - $newHeight;
+
+        $x = $x < 0 ? $x / 2 : $x;
+        $y = $y < 0 ? $y / 2 : $y;
+
+        $finalImage = imagecreatetruecolor($coverWidth, $coverHeight);
+        switch($newCover['type']) {
+          case 'image/jpeg':
+          case 'image/jpg':
+            $image = imagecreatefromjpeg($newCover['tmp_name']);
+          break;
+          case 'image/png':
+            $image = imagecreatefrompng($newCover['tmp_name']);
+          break;
+        }
+
+        imagecopyresampled(
+          $finalImage, $image,
+          $x, $y, 0, 0,
+          $newWidth, $newHeight, $originalWidth, $originalHeight
+        );
+
+        $coverName = md5(time().mt_rand(0, 9999)).'.jpg';
+
+        imagejpeg($finalImage, './media/covers/'.$coverName, 100);
+
+        $user->cover = $coverName;
+      }
+    }
+
     $userDao->update($user);
   }
 
